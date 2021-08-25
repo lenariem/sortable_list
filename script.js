f5c741c4a3bf00b1a25c0d3c88875bd7487ceaf4
unknown <olena.denysova88@gmail.com>
2021-08-25 12:54:15 +0200

    scramble list items

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ const largestCountries = [
 
 // Store list items
 const listItems = [];
+console.log(listItems)
 
 let dragStartIndex;
 
@@ -23,16 +24,20 @@ createList();
 
 // Insert list items into DOM
 function createList() {
-  [...largestCountries].forEach((country, index) => {
-    const listItem = document.createElement('li');
-    listItem.setAttribute('data-index', index);
-    listItem.innerHTML = `
-      <span class="number">${index + 1}</span>
-      <div class="draggable" draggable="true">
-        <p class="country-name">${country}</p>
-        <i class="fas fa-grip-lines"></i>
-      </div>
-    `;
+  [...largestCountries]
+    .map(item => ({value: item, sort: Math.random()}))
+    .sort((a, b) => a.sort - b.sort)
+    .map(item => item.value)
+    .forEach((country, index) => {
+      const listItem = document.createElement('li');
+      listItem.setAttribute('data-index', index);
+      listItem.innerHTML = `
+        <span class="number">${index + 1}</span>
+        <div class="draggable" draggable="true">
+          <p class="country-name">${country}</p>
+          <i class="fas fa-grip-lines"></i>
+        </div>
+      `;
 
     listItems.push(listItem);
 
