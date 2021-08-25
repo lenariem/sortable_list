324c7bd1e48d27f0d264ca7c90a741cb7ff38cab
unknown <olena.denysova88@gmail.com>
2021-08-25 12:34:54 +0200

    insert list items into dom

---
+++ script.js
@@ -0,0 +1,42 @@
+const draggable_list = document.getElementById('draggable-list');
+const check = document.getElementById('check');
+
+const largestCountries = [
+  'Russia (17,098,242)',
+  'Canada (9,984,670)',
+  'United States (9,826,675)',
+  'China (9,596,961)',
+  'Brazil (8,514,877)',
+  'Australia (7,741,220)',
+  'India (3,287,263)',
+  'Argentina (2,780,400)',
+  'Kazakhstan	(2,724,900)',
+  'Algeria (2,381,741)'
+];
+
+// Store list items
+const listItems = [];
+
+let dragStartIndex;
+
+createList();
+
+// Insert list items into DOM
+function createList() {
+  [...largestCountries].forEach((country, index) => {
+    const listItem = document.createElement('li');
+    listItem.setAttribute('data-index', index);
+    listItem.innerHTML = `
+      <span class="number">${index + 1}</span>
+      <div class="draggable" draggable="true">
+        <p class="country-name">${country}</p>
+        <i class="fas fa-grip-lines"></i>
+      </div>
+    `;
+
+    listItems.push(listItem);
+
+    draggable_list.appendChild(listItem);
+  })
+
+}
